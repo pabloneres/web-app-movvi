9b173a528737f1814bcd36597baf35f3d2553bca
Pablo Neres <48608017+pabloneres@users.noreply.github.com>
2020-03-27 03:12:34 -0300

    Updates

---
+++ precache-manifest.468441fc2b986a4db0e528e746423134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0335bc52e7bcf468c750b7bef15efe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242e064d6635cf674d95",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "64475f86d0dcb67ee5b2",
+    "url": "/static/css/main.a5db3d11.chunk.css"
+  },
+  {
+    "revision": "242e064d6635cf674d95",
+    "url": "/static/js/2.bd077726.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/static/js/2.bd077726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64475f86d0dcb67ee5b2",
+    "url": "/static/js/main.6d55fb18.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  }
+]);
