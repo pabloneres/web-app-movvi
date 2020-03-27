d043a95a1e511e562332af9af82e0bd1fe1cf3c1
Pablo Neres <48608017+pabloneres@users.noreply.github.com>
2020-03-27 03:21:31 -0300

    Updates

---
+++ precache-manifest.fcb1d20c2bc8b3fcf71f61b82caf66fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d509efb123751ea05e7469a76833e8c",
+    "url": "/web-app-movvi/index.html"
+  },
+  {
+    "revision": "242e064d6635cf674d95",
+    "url": "/web-app-movvi/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "64475f86d0dcb67ee5b2",
+    "url": "/web-app-movvi/static/css/main.a5db3d11.chunk.css"
+  },
+  {
+    "revision": "242e064d6635cf674d95",
+    "url": "/web-app-movvi/static/js/2.bd077726.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/web-app-movvi/static/js/2.bd077726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64475f86d0dcb67ee5b2",
+    "url": "/web-app-movvi/static/js/main.6d55fb18.chunk.js"
+  },
+  {
+    "revision": "d58244373fe8b1d42302",
+    "url": "/web-app-movvi/static/js/runtime-main.3dc84f2e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.468441fc2b986a4db0e528e746423134.js"
+  "/web-app-movvi/precache-manifest.fcb1d20c2bc8b3fcf71f61b82caf66fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-app-movvi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
